38ffe1140cd0f3ad01a6d30fe573047b324eebae
faysal909 <faysalofficials909@gmail.com>
2022-01-28 18:04:15 +0600

    aother testfile

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/*const element = React.createElement('h1',null,'hello world');
+const element2 = <h2>hello this is from element2</h2>;
+console.log(element2);
+console.log(element);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function grettings (name) { 
+ if(name){
+   return <h1>hello, {name} </h1>
+ } 
+  return <h1>hello buddy</h1>
+ };
+ console.log(grettings());
+ const element3 = <h3>hello{grettings('faysal')}</h3>
+ console.log(element3);
+
+//  multiline jsx
+*/
+const element4 = (
+  <h1 className="heading">
+    hello
+    <span><h2>hello hi</h2>hiiiiiiiiiii</span>
+    <img src="" alt="" srcset="" />
+  </h1>
+  );
+console.log(element4);
